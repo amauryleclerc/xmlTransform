8db9c7dd1bc69a6542b9a06ddc1414313a3c2fb9
Amaury LECLERC <amauryleclerc@hotmail.fr>
2014-12-03 17:02:41 +0100

    creation de setXML dans dom

--- src/dom/BiblioToXhtmlDom.java
+++ src/dom/BiblioToXhtmlDom.java
@@ -1,5 +1,48 @@
 package dom;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 public class BiblioToXhtmlDom {
+	private Document outHTML = null;
+	private NodeList conferences = null;
+
+	public static void main(String args[]) {
+		String xmlPath = System.getProperty("user.dir")
+				+ "/TALN-RECITAL-BIB.xml";
+		BiblioToXhtmlDom biblioToXhtmlDom = new BiblioToXhtmlDom();
+		try {
+			biblioToXhtmlDom.setXml(xmlPath);
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			System.out.println("ERREUR de parse XML"+ e.getMessage());
+			e.printStackTrace();
+		}
+		biblioToXhtmlDom.createDivIndex();
+
+	}
+
+	public void setXml(String xmlPath) throws ParserConfigurationException, SAXException, IOException {
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		dbf.setNamespaceAware(true);
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		Document doc = db.parse(new File(xmlPath));
+		this.conferences = doc.getElementsByTagName("conferences").item(0).getChildNodes();
+	}
+
+	private void createDivIndex() {
+		System.out.println(conferences.getLength());
+	}
+
+	private void createDivDetail() {
+
+	}
 
 }
